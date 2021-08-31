350e6c36357dfb244f13843112f909516a3521a0
vyeh852 <vyeh852@gmail.com>
2021-08-31 16:35:13 +0800

    add pages and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-function App() {
+import React from "react";
+import {Route,Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart"
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import Sidemenu from "./components/Sidemenu";
+
+export default function App() {
+  const [menuState,toggleMenuState] = React.useState(false)
   return (
     <div className="App">
-      <h2 className="text-2xl">React</h2>
+      <Navbar toggleMenu={()=>{toggleMenuState(!menuState)}}/>
+      <Sidemenu menuState={menuState} toggleMenu={()=>{toggleMenuState(!menuState)}}/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/cart" component={Cart}/>
+        <Route path="/product/:id" component={Product}/>
+      </Switch>
     </div>
   );
 }
 
-export default App;
